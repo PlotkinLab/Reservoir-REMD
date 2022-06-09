938dbafc437f63c9350633dcd6d1bc03635f5edc
shawnhsueh <cttm4a1@gmail.com>
2022-06-08 20:09:05 -0700

    polish

--- example/Alanine-dipeptide/run.sh
+++ example/Alanine-dipeptide/run.sh
@@ -6,5 +6,5 @@ do
 	grompp_mpi -f md.mdp -c prot-solv-ion-em-nvt-npt.gro -t state.cpt -p topol.top -o md.tpr; cd ..
 done
 
-#Run a Res-REMD with three replicas. The checkpoint files, element{1..100}.cpt, are placed in the folder that hosts the highest-temperature replica (i.e. sim3).
+#Run Res-REMD. The checkpoint files, element{1..100}.cpt, are placed in the folder that hosts the highest-temperature replica (i.e. sim3).
 mpirun -n 5 mdrun_mpi -deffnm md -v -multidir sim{1..3} -replex 500 -reservoir element{1..10}.cpt
